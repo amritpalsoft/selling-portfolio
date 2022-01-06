2ab4dbf53f01518e4e6957b9ecee36b568c3141c
amrit <amritpalsoftware@gmail.com>
2022-01-06 21:46:39 +0530

    initial commit

---
+++ precache-manifest.8b71356be083f085085f7edf3a26165b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7367d20a5b27ac571c4fc1f6cd8b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9be4026ee315010583b",
+    "url": "/static/css/2.655bca66.chunk.css"
+  },
+  {
+    "revision": "ff4a4a62ea93bf464528",
+    "url": "/static/css/main.13467de3.chunk.css"
+  },
+  {
+    "revision": "a9be4026ee315010583b",
+    "url": "/static/js/2.1f9a8528.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.1f9a8528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4a4a62ea93bf464528",
+    "url": "/static/js/main.fc03d67c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "7d32b14f53f0f2363c4355948920c21a",
+    "url": "/static/media/resume-sample_compressed (1).7d32b14f.pdf"
+  }
+]);
